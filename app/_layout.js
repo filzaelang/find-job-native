87d4e807486fdc60a14aa7820c71c1097cbb1594
Filza Elang Buana <filzaelang@gmail.com>
2024-05-21 09:33:27 +0700

    second commit

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,35 @@
 import { Stack } from "expo-router"
+import { useCallback } from "react"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import { View } from "react-native"
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
-    return (
-        <Stack />
-    )
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    console.log("fontsLoaded status = ", fontsLoaded)
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])
+
+    if (fontsLoaded === false) return null
+
+    return <Stack onLayout={onLayoutRootView} />
+
+    // return (
+    //     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+    //         <Stack />
+    //     </View>
+    // )
 }
 
 export default Layout
